3daec80827da2bb6e3561b64d018ee229b226722
unknown <damian1234567812@gmail.com>
2023-01-28 19:54:26 +0100

    final

---
+++ src/main/java/com/TOiK/Project/ModelRes.java
@@ -0,0 +1,79 @@
+package com.TOiK.Project;
+
+public class ModelRes {
+    private String name;
+    private String surname;
+    private String email;
+    private String table;
+    private String time;
+    private String day;
+    private String month;
+    private String year;
+
+    public ModelRes() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
